b3fa5763153203fd7916a65ee6f6f9cfd0e20e2d
Víctor Gómez <39098525+victorgomez09@users.noreply.github.com>
2025-04-23 06:57:36 +0000

    finish weather component

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,21 @@
-import { provideEventPlugins } from "@taiga-ui/event-plugins";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ArrowUp, LucideAngularModule } from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideEventPlugins()]
+  providers: [
+    provideAnimations(),
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideEventPlugins(),
+    importProvidersFrom(LucideAngularModule.pick({ ArrowUp })),
+  ],
 };
--- frontend/src/app/core/components/wheather/wheather.component.ts
+++ frontend/src/app/core/components/wheather/wheather.component.ts
@@ -1,19 +1,19 @@
+import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   inject,
   OnInit,
 } from '@angular/core';
+import { LucideAngularModule, ArrowUp } from 'lucide-angular';
+
 import { WebSocketService } from '../../../services/web-socket/web-socket.service';
 import { WeatherData } from '../../models/weather';
-import { CommonModule } from '@angular/common';
-import { GaugeComponent } from '../gauge/gauge.component';
 
 @Component({
   selector: 'app-wheather',
-  imports: [CommonModule, GaugeComponent],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './wheather.component.html',
-  styleUrl: './wheather.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WheatherComponent implements OnInit {
@@ -43,11 +43,38 @@ export class WheatherComponent implements OnInit {
     }
   }
 
+  handleRain(rainfall: number | undefined) {
+    if (rainfall === 1) return 'Yes';
+    if (rainfall === 0) return 'No';
+
+    return undefined;
+  }
+
   get airTemp() {
     return Number(this.weather.AirTemp);
   }
 
   get trackTemp() {
     return Number(this.weather.TrackTemp);
+  }
+
+  get humidity() {
+    return Number(this.weather.Humidity);
+  }
+
+  get pressure() {
+    return Number(this.weather.Pressure);
+  }
+
+  get rainfall() {
+    return Number(this.weather.Rainfall);
+  }
+
+  get windSpeed() {
+    return Number(this.weather.WindSpeed);
+  }
+
+  get windDirection() {
+    return Number(this.weather.WindDirection);
   }
 }
